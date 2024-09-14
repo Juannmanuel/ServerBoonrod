e1478696190d1f9497fc8f05e6f1ab00a4bb4057
Juan Manuel <losredondos551@gmail.com>
2024-09-13 22:37:33 -0300

    PORT

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const server = require("./src/server");
-const { conn, Country } = require('./src/db.js');
+const { conn } = require('./src/db.js');
 
 // const PORT = 3001; 
 const PORT = process.env.PORT || 3001;
