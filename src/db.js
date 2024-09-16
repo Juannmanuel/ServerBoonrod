8b4a2e482beeebdc6cfadf939b29bdeb96898a3a
Juan Manuel <losredondos551@gmail.com>
2024-09-16 15:33:39 -0300

    db connect

--- src/db.js
+++ src/db.js
@@ -8,7 +8,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/boonrod`, {
   logging: false, 
 });
 
